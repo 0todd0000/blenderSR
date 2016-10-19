9b28b0d33a8ffc6fb3a7bce8b994fe520448d4c4
Todd Pataky <0todd0@gmail.com>
2016-10-19 14:40:29 +0900

    minor user settings update

--- blenderSR/user_settings.py
+++ blenderSR/user_settings.py
@@ -3,7 +3,7 @@
 path2blender   = '/Applications/Blender/v2.76/Blender.app/Contents/MacOS/blender'
 
 paths2add      = []
-paths2add.append(  '/Users/todd/WorkingCopies/feblender/trunk'  )
+paths2add.append(  '/Users/todd/WorkingCopies/feblender.git/trunk'  )
 
 
 
